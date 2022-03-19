dc467a4ca8378cbc7b4a90cfcdbbacf6f3fdd7bd
Your Name <you@example.com>
2022-03-19 19:48:08 -0300

    new try

--- list.c
+++ list.c
@@ -48,8 +48,8 @@ void * firstList(List * list) {
 
 void * nextList(List * list) {
   if (list->current == NULL) return NULL;
-  list->current = list->current->next;
   return list->current->data;
+  list->current = list->current->next;
 }
 
 void * lastList(List * list) {
